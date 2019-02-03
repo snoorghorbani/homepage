2e3b4952ada85dee0209cbe345f0d72b1b200bff
soushians noorghorbani <snoorghorbani@gmail.com>
2019-02-03 22:26:25 +0330

    .

--- src/circle-wave.1.ts
+++ src/circle-wave.1.ts
@@ -25,7 +25,7 @@ interface IConfig {
 	gap: number;
 }
 
-export const circleWave = function (scene, config: IConfig) {
+export const circleWave = function(scene, config: IConfig) {
 	var _w = window.innerWidth;
 	var _h = window.innerHeight;
 
@@ -33,75 +33,71 @@ export const circleWave = function (scene, config: IConfig) {
 		mouseY = _h / 2;
 	var mouseAngle = Utility.angle.toRad(90);
 
+	var tweenEasing = TWEEN.Easing.Circular.Out;
+
+	var circleGeo = new THREE.Geometry();
+	var startpoints = Utility.geometry.randomPointsInObject(new THREE.SphereGeometry(20, 20, 20), 3000);
+	for (var i = 0; i < startpoints.length; i++) {
+		circleGeo.vertices.push(startpoints[i]);
+	}
+	for (var i = 0; i < circleGeo.vertices.length; i += 1) {
+		// circleGeo.colors[i] = new THREE.Color(Math.random(), Math.random(), Math.random());
+		circleGeo.colors[i] = new THREE.Color(Math.random(), Math.random(), Math.random());
+		// circleGeo.colors[i + 1] = circleGeo.colors[i];
+	}
 	var circle = new THREE.Line(
-		new THREE.Geometry(),
+		circleGeo,
 		new THREE.LineBasicMaterial({
-			color: config.color,
+			// color: config.color,
+			vertexColors:  THREE.VertexColors,
 			linewidth: config.lineWidth,
 			transparent: true
 		})
 	);
-	var tweenEasing = TWEEN.Easing.Circular.Out;
 
-	var startpoints = Utility.geometry.randomPointsInObject(new THREE.SphereGeometry(20, 20, 20), 297);
 	function buildCircle() {
-		for (var i = 0; i < startpoints.length; i++) {
-			circle.geometry.vertices.push(startpoints[i]);
-		}
 		scene.add(circle);
 	}
 	var positions;
 	var points = [
-		Utility.geometry.randomPointsInObject(new THREE.SphereGeometry(11, 11, 11), 297),
-		Utility.geometry.randomPointsInObject(new THREE.CubeGeometry(11, 11, 11), 297),
-		Utility.geometry.randomPointsInObject(new THREE.CylinderGeometry(11, 11, 11), 297),
-		Utility.geometry.randomPointsInObject(new THREE.CubeGeometry(11, 11, 11), 297)
-	]
+		Utility.geometry.randomPointsInObject(new THREE.SphereGeometry(11, 11, 11), 3000),
+		Utility.geometry.randomPointsInObject(new THREE.CubeGeometry(11, 11, 11), 3000),
+		Utility.geometry.randomPointsInObject(new THREE.CylinderGeometry(11, 6, 22), 3000),
+		Utility.geometry.randomPointsInObject(new THREE.TetrahedronGeometry(11), 3000)
+	];
 	function updatePoints() {
-
-		// var geos = [
-		// 	new THREE.SphereGeometry(11, 11, 11),
-		// 	new THREE.CubeGeometry(11, 11, 11),
-		// 	new THREE.CylinderGeometry(11, 11, 11),
-		// 	new THREE.CubeGeometry(11, 11, 11),
-		// ]
 		debugger;
-		// positions = new_positions = Utility.geometry.randomPointsInObject(geos[Math.floor(Utility.angle.toDeg(mouseAngle) / 90)], 297);
-		positions =  points[Math.floor(Utility.angle.toDeg(mouseAngle) / 90)];
+		// positions = new_positions = Utility.geometry.randomPointsInObject(geos[Math.floor(Utility.angle.toDeg(mouseAngle) / 90)], 3000);
+		positions = points[Math.floor(Utility.angle.toDeg(mouseAngle) / 90)];
 		circle.geometry.verticesNeedUpdate = true; // do not forget to mark this flag each time you want to update geometry
-
-		var colors = [
-			0x0000ff,
-			0x00ffff,
-			0xff00ff,
-			0x00ff00,
-		]
-		circle.material.setValues({
-			color:colors[Math.floor(Utility.angle.toDeg(mouseAngle) / 90)]
-		});
-
 	}
 
 	function startTweens() {
-
 		for (let i = 0; i < positions.length; i++) {
 			if (
 				circle.geometry.vertices[i].x == positions[i].x &&
 				circle.geometry.vertices[i].y == positions[i].y &&
 				circle.geometry.vertices[i].z == positions[i].z
 			) {
-debugger;
+				debugger;
 			} else {
 				new TWEEN.Tween(circle.geometry.vertices[i])
-					.to({
-						x: positions[i].x,
-						y: positions[i].y,
-						z: positions[i].z
-					},
+					.to(
+						{
+							x: positions[i].x,
+							y: positions[i].y,
+							z: positions[i].z
+						},
 						config.tweenDelay
 					)
 					.easing(tweenEasing)
 					.start();
+				var colors = [ 0x0000ff, 0x00ffff, 0xff00ff, 0x00ff00 ];
+
+				// new TWEEN.Tween(circle.material.color)
+				// 	.to(new THREE.Color(colors[Math.floor(Utility.angle.toDeg(mouseAngle) / 90)]), config.tweenDelay)
+				// 	.easing(tweenEasing)
+				// 	.start();
 			}
 		}
 	}
@@ -113,12 +109,11 @@ debugger;
 		requestAnimationFrame(loop);
 	}
 
-	document.body.onmousemove = function (event) {
+	document.body.onmousemove = function(event) {
 		debugger;
 		mouseAngle = Utility.angle.getAngle(event.pageX, event.pageY);
 		startTweens();
 	};
-
 
 	buildCircle();
 	loop();
